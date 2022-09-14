826134380a474a88000fc53cd91cf4968b77e1a6
Ibni Shaquille <alleibrahim1710@gmail.com>
2022-09-14 16:58:09 +0700

    Tugas 2 sudah selesai

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Create your views here.
+def show_wishlist(request):
+    data_barang_wishlist = CatalogItem.objects.all()
+    context = {
+        'list_items': data_barang_wishlist,
+        'nama': 'Ibni Shaquille Syauqi Ibrahim',
+        'studentId': '2106706735'
+    }
+    return render(request, "katalog.html", context)
