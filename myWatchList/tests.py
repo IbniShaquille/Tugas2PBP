6721a27ac7c2e9f13639b107c78ae444070854f4
Ibni Shaquille Syauqi Ibrahim <alleibrahim1710@gmail.com>
2022-09-22 11:57:02 +0700

    fix test error

--- myWatchList/tests.py
+++ myWatchList/tests.py
@@ -2,14 +2,14 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 class MyWatchListTest(TestCase):
-    def html_test(self):
-        urls_test = Client().get('/myWatchList/html')
+    def test_html(self):
+        urls_test = Client().get('/mywatchlist/html/')
         self.assertEqual(urls_test.status_code, 200)
 
-    def xml_test(self):
-        urls_test = Client().get('/myWatchList/xml')
+    def test_xml(self):
+        urls_test = Client().get('/mywatchlist/xml/')
         self.assertEqual(urls_test.status_code, 200)
         
-    def json_test(self):
-        urls_test = Client().get('/myWatchList/json')
+    def test_json(self):
+        urls_test = Client().get('/mywatchlist/json/')
         self.assertEqual(urls_test.status_code, 200)
